311fa7b14a5637584329eada81e4b75aa0327e9f
hoodlaptop <165354489+hoodlaptop@users.noreply.github.com>
2026-03-05 19:38:56 +0900

    Assignment_03 / No advanced stage applied

---
+++ Assignment_03/Assignment_03.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include "Inventory.h"
+
+using namespace std;
+
+class Item
+{
+public:
+    Item() : name_(""), price_(0) {}
+    Item(const string& name, int price) : name_(name), price_(price) {}
+
+    void PrintInfo() const
+    {
+        cout << "[이름: " << name_ << ", 가격: " << price_ << "G]" << endl;
+    }
+
+private:
+    string name_;
+    int price_;
+};
+
+int main(int argc, char* argv[])
+{
+    cout << "=== Inventory 테스트 ===" << endl << endl;
+    
+    Inventory<Item> inventory(5);
+
+    cout << "최대 용량: " << inventory.GetCapacity() << endl;
+    cout << "현재 아이템 수: " << inventory.GetSize() << endl << endl;
+
+    cout << "--- 아이템 추가 ---" << endl;
+    inventory.AddItem(Item("장검", 100));
+    inventory.AddItem(Item("방패", 80));
+    inventory.AddItem(Item("체력 포션", 30));
+
+    cout << "현재 아이템 수: " << inventory.GetSize() << endl << endl;
+
+    cout << "--- 전체 아이템 목록 ---" << endl;
+    inventory.PrintInfo();
+    cout << endl;
+
+    cout << "--- 마지막 아이템 제거 ---" << endl;
+    inventory.RemoveLastItem();
+    cout << "현재 아이템 수: " << inventory.GetSize() << endl;
+    inventory.PrintInfo();
+    cout << endl;
+    
+    cout << "--- 인벤토리 가득 채우기 테스트 ---" << endl;
+    inventory.AddItem(Item("활", 120));
+    inventory.AddItem(Item("갑옷", 200));
+    inventory.AddItem(Item("투구", 90));
+    inventory.AddItem(Item("신발", 60));  // 6번째 - 실패해야 함
+
+    cout << "현재 아이템 수: " << inventory.GetSize() << endl << endl;
+
+    cout << "--- 빈 인벤토리 테스트 ---" << endl;
+    Inventory<Item> emptyInventory(3);
+    emptyInventory.RemoveLastItem();  // 비어있으므로 경고 메시지
+
+    cout << endl << "--- 0 이하 용량 테스트 ---" << endl;
+    Inventory<Item> minInventory(-5);
+    cout << "입력 용량: -5, 실제 용량: " << minInventory.GetCapacity() << endl;
+
+    return 0;
+}
+
---
+++ Assignment_03/Inventory.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+#include <iostream>
+
+template <typename T>
+class Inventory
+{
+public:
+    Inventory(int capacity = 10);
+    
+    ~Inventory();
+    
+    void AddItem(const T& item);
+    void RemoveLastItem();
+    int GetSize() const;
+    int GetCapacity() const;
+    void PrintInfo() const;
+
+private:
+    T* pItems_;
+    int capacity_;
+    int size_;
+};
+
+template <typename T>
+Inventory<T>::Inventory(int capacity)
+{
+    if (capacity <= 0)
+    {
+        capacity = 1;
+    }
+
+    capacity_ = capacity;
+    size_ = 0;
+    pItems_ = new T[capacity_];
+}
+
+template <typename T>
+Inventory<T>::~Inventory()
+{
+    delete[] pItems_;
+    pItems_ = nullptr;
+}
+
+template <typename T>
+void Inventory<T>::AddItem(const T& item)
+{
+    if (size_ >= capacity_)
+    {
+        std::cout << "인벤토리가 꽉 찼습니다!" << std::endl;
+        return;
+    }
+
+    pItems_[size_] = item;
+    size_++;
+}
+
+template <typename T>
+void Inventory<T>::RemoveLastItem()
+{
+    if (size_ <= 0)
+    {
+        std::cout << "인벤토리가 비어있습니다." << std::endl;
+        return;
+    }
+
+    size_--;
+}
+
+template <typename T>
+int Inventory<T>::GetSize() const
+{
+    return size_;
+}
+
+template <typename T>
+int Inventory<T>::GetCapacity() const
+{
+    return capacity_;
+}
+
+template <typename T>
+void Inventory<T>::PrintInfo() const
+{
+    for (int i = 0; i < size_; i++)
+    {
+        pItems_[i].PrintInfo();
+    }
+}
